da027a385129015612bfcad08169a9fbbbf2ed6a
Akhatov Artur <artzer592@gmail.com>
2021-07-25 15:42:00 +0500

    add btn add  prog lang

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Abanking_employees.Data;
+using Abanking_employees.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Abanking_employees.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        [HttpGet]
+        public ActionResult Add()
+        {
+            AppDBContext db = new AppDBContext();
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Department department)
+        {
+            AppDBContext db = new AppDBContext();
+
+            db.Entry(department).State = EntityState.Added;
+            db.SaveChanges();
+
+            return RedirectPermanent("/Home/index");
+        }
+    }
+}
---
+++ Controllers/ProgrammingLanguageController.cs
@@ -0,0 +1,33 @@
+﻿using Abanking_employees.Data;
+using Abanking_employees.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Abanking_employees.Controllers
+{
+    public class ProgrammingLanguageController : Controller
+    {
+        [HttpGet]
+        public ActionResult Add()
+        {
+            AppDBContext db = new AppDBContext();
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(ProgrammingLanguage progLang)
+        {
+            AppDBContext db = new AppDBContext();
+
+            db.Entry(progLang).State = EntityState.Added;
+            db.SaveChanges();
+
+            return RedirectPermanent("/Home/index");
+        }
+    }
+}
